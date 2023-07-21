f734a4272a0c0679f57b54244085fe8d8a1eee44
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-21 21:15:07 +0400

    display each comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create comment_params
-    redirect_to @article
-  end
 
+    @comment = @article.comments.new comment_params
+
+    if @comment.save
+      redirect_to @article
+    else
+      render :show
+    end
+
+  end
+  
   private
   def comment_params
     params.require(:comment).permit(:author, :body)
