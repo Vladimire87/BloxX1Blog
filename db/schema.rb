96c77027280343febb0bb9060fb6bc5d14ca5129
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-26 14:19:20 +0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_103311) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_101740) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
