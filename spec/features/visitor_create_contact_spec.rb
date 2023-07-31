06e8522d703b89405f28a9bc200a6aeb7e570e4d
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-31 15:22:15 +0400

    update tests (allows a guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,4 +5,11 @@ feature "Contact Creation" do
     visit new_contacts_path
     expect(page).to have_content I18n.t "contacts.contact_us"
   end
+  scenario "allows a guest to create contact" do
+    visit new_contacts_path
+    fill_in :contact_email, with: "example@mail.com"
+    fill_in :contact_message, with: "Text"
+    click_button "Submit"
+    expect(page).to have_content "Thank you! Your message was sent."
+  end
 end
