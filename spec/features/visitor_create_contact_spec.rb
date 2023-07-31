41faa65197195c82039a87bdea741c4e36526648
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-31 15:01:03 +0400

    test for I18n

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 feature "Contact Creation" do
   scenario "allows acess to contacts page" do
     visit new_contacts_path
-    expect(page).to have_content "Cheers, Vladimir Egorov"
+    expect(page).to have_content I18n.t "contacts.contact_us"
   end
 end
