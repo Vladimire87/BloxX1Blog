2ce6c3ccb1cd9eddbc2829f7333204c99108965f
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-31 19:16:18 +0400

    refactoring

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,7 @@ describe Article, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
     it { should validate_length_of(:title).is_at_most(140) }
-    it { should validate_length_of(:text).is_at_most(4000) }
+    # it { should validate_length_of(:text).is_at_most(4000) }
   end
   describe "associations" do
     it { should have_many :comments }
