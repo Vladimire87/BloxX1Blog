60a1c3df5082b4266d872c898c9e177d19a5d104
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-02 14:39:56 +0400

    update session helper

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,10 +1,10 @@
 def sign_up
   visit new_user_registration_path
 
-  fill_in :user_username, with: "userTester"
-  fill_in :user_email, with: "example@mail.com"
-  fill_in :user_password, with: "12345678"
-  fill_in :user_password_confirmation, with: "12345678"
+  fill_in :floatingUsername, with: "userTester"
+  fill_in :floatingEmail, with: "example@mail.com"
+  fill_in :floatingPassword, with: "12345678"
+  fill_in :floatingPasswordConfirmation, with: "12345678"
 
   click_button "Sign up"
 end
